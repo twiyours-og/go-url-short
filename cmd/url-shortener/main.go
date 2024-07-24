5380e4bd50d99bf0010c7aa8ce9c34dce10fc49d
jusnamthis <KartashovVD@yandex.ru>
2024-07-25 01:31:36 +0700

    config, log, storage initialization

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"URL-SHORTENER/internal/config"
+	"URL-SHORTENER/internal/lib/sl"
+	"URL-SHORTENER/internal/storage/sqlite"
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+	log.Info("starting URL shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled!")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("Failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+
+	//TODO: init router
+
+	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelInfo,
+			}),
+		)
+	}
+
+	return log
+}
